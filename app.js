777cfdd97636b793a6bbeb4856c3146704c2ae15
Brian Warner <brianwarner@Brians-MacBook-Pro.local>
2020-09-01 10:34:08 -0400

    added score to quiz

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const correctAnswers = ['C', 'A', 'B', 'C'];
-
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -15,5 +15,30 @@ form.addEventListener('submit', e => {
         }
     });
 
-    
+    // show result on page
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
 })
+
+// let i = 0;
+// const timer = setInterval(() => {
+// i++;
+// if(i === 5) {
+//     clearInterval(timer)
+// }
+// }, 1000);
+
+// setTimeout(() => {
+//     alert('')
+// }, 3000)
