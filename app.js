ee202c9e18099c6d1c3c6c044f29a97d93ef0493
Brian Warner <brianwarner@Brians-MacBook-Pro.local>
2020-09-01 10:07:32 -0400

    added the ability to record the answers and keep score in the JS file

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const correctAnswers = ['B', 'B', ]
+const correctAnswers = ['C', 'A', 'B', 'C'];
+
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.question1.value, form.question2.value, form.question3.value, form.question4.value];
+
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+
+    
+})
